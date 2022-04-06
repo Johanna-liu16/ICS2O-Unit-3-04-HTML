51a9095c59e18fb206496127827d2d81d380a533
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-04-06 12:14:40 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2020 Johanna Liu All rights reserved
+//
+// Created by: Johanna Liu
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area of a parrallelogram.
+ */
+function calculate () {
+  // input
+  const length = parseInt(document.getElementById("length-of-pyramid").value)
+  const width = parseInt(document.getElementById("width-of-pyramid").value)    
+  const height = parseInt(document.getElementById("height-of-pyramid").value)
+
+  // process
+  const volume = (( length * width * height) / 3 )
+
+  // output
+  document.getElementById("answers").innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
+}
