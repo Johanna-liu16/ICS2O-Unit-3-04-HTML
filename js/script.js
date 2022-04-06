2e28204ea4b5c47923bd06b206b75b41bb5b1029
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-04-06 16:48:32 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Johanna Liu All rights reserved
+// Copyright (c) 2022 Johanna Liu All rights reserved
 //
 // Created by: Johanna Liu
 // Created on: Mar 2022
@@ -10,13 +10,11 @@
  */
 function calculate () {
   // input
-  const length = parseInt(document.getElementById("length-of-pyramid").value)
-  const width = parseInt(document.getElementById("width-of-pyramid").value)    
-  const height = parseInt(document.getElementById("height-of-pyramid").value)
+  const F = parseInt(document.getElementById("fahrenheit-degrees").value)
 
   // process
-  const volume = (( length * width * height) / 3 )
+  const celsius = ((F − 32) × 5/9)
 
   // output
-  document.getElementById("answers").innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
+  document.getElementById("answers").innerHTML = 'The degrees in celsius is: ' + celsius.toFixed(2) + ' °C'
 }
